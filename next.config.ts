12da821364516a3171f0cc0e10fd7b73e63d70fc
akdevspace-crypto <akdevspace@gmail.com>
2026-03-23 17:02:55 +0530

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,21 +1,25 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-    devIndicators: false,
-    images: {
-        remotePatterns: [
-            {
-                protocol: 'https',
-                hostname: 'images.unsplash.com',
-                pathname: '/**',
-            },
-            {
-                protocol: 'https',
-                hostname: 'i.pravatar.cc',
-                pathname: '/**',
-            }
-        ],
-    },
+  output: 'export', // ✅ CRITICAL
+
+  devIndicators: false,
+
+  images: {
+    unoptimized: true, // ✅ REQUIRED for static export
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'i.pravatar.cc',
+        pathname: '/**',
+      }
+    ],
+  },
 };
 
 export default nextConfig;
